98417c433a543e1861b747246c426cb63e3b0122
takumi-kusaka <kusakataku1189@gmail.com>
2026-01-08 09:40:25 +0900

    Program.cs を更新

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿int x = 10;
+int x = 10;
 int y = 3;
-Console.WriteLine(x + " + " + y + " = " + (x + y));
-Console.WriteLine(x + " - " + y + " = " + (x - y));
-Console.WriteLine(x + " × " + y + " = " + (x * y));
-Console.WriteLine(x + " ÷ " + y + " = " + (x / y));
+Console.WriteLine(x + y);
+Console.WriteLine(x - y);
+Console.WriteLine(x * y);
+Console.WriteLine(x / y);
